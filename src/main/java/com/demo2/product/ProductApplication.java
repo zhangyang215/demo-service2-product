028c43654795cc5db9743689430a4786d52271a5
fangang <fangang@DESKTOP-7DODCNH>
2020-03-02 13:42:02 +0800

    enable hystrix client

--- src/main/java/com/demo2/product/ProductApplication.java
+++ src/main/java/com/demo2/product/ProductApplication.java
@@ -7,7 +7,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -21,9 +21,9 @@ import org.springframework.context.annotation.ImportResource;
 @ComponentScan(basePackages={"com.demo2"})
 @ImportResource(locations={"classpath*:applicationContext-*.xml"})
 @MapperScan("com.demo2.support.dao")
-@EnableEurekaClient
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableHystrix
 public class ProductApplication {
 
 	/**
